e49c8936427b23226d0f78fb515c0a2416af29c9
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2022-05-23 02:17:30 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,71 +1,92 @@
-// Esperamos el contenido del elemento de ventana para hacer las operaciones 
-window.addEventListener('load', ()=>{ 
-		
-	redimensionar(); // Redimensionamos el tamaño del canvas al cargar la Página 
-	document.addEventListener('mousedown', iniciarDibujado); 
-	document.addEventListener('mouseup', detenerDibujado); 
-	document.addEventListener('mousemove', dibujo); 
-	window.addEventListener('resize', redimensionar); 
-}); 
-	
-// Seleccionamos el id del elemento 
-const canvas = document.querySelector('#micanvas'); 
+ const canvas = document.querySelector('#micanvas');
 
-// Contexto del canva para operaciones 2d 
-const ctx = canvas.getContext('2d'); 
-	
-// Redimensionamos el tamaño del canvas según el tamaño de la ventana 
-function redimensionar(){ 
-	ctx.canvas.width = window.innerWidth; 
-	ctx.canvas.height = window.innerHeight; 
-} 
-	
-// Colocamos el curos en su posición inicial 
-let coordenadas = {x:0 , y:0}; 
+ const contenedor = canvas.getContext('2d');
 
-// Con esta variable se inicia deshabilitado el dibujado en el Canvas 
-let dibujar = false; 
-	
-// Actualizamos las coordenadas del cursor cuando un evento se dispara
-function obtenerPosicion(event){ 
-	coordenadas.x = event.clientX - canvas.offsetLeft; 
-	coordenadas.y = event.clientY - canvas.offsetTop; 
-} 
+ // Usamos todo el alto y ancho disponible para el canvas
+ canvas.height = window.innerHeight;
+ canvas.width = window.innerWidth;
 
-// Habilitamos el dibujado en el Canvas  
-function iniciarDibujado(event){ 
-	dibujar = true; 
-	obtenerPosicion(event); 
-} 
+ // Ancho del Trazo 
+ contenedor.lineWidth = 5;
 
-// Detenemos el dibujado 
-function detenerDibujado(){ 
-	dibujar = false; 
-} 
-	
-function dibujo(event){ 
-	
-	if (!dibujar) return; 
+ // Trazo redondeado 
+ contenedor.lineCap = 'round';
 
-	ctx.beginPath(); 
-		
-	ctx.lineWidth = 5; 
+ // Color del trazo del dibujo en el Canvas 
+ contenedor.strokeStyle = '#5858ef';
 
-	// Trazo redondeado 
-	ctx.lineCap = 'round'; 
-		
-	// Color del trazo del dibujo en el Canvas 
-	ctx.strokeStyle = '#5858ef'; 
-		
-	// El cursor para comenzar a dibujar se mueve a esta coordenada 
-	ctx.moveTo(coordenadas.x, coordenadas.y); 
+ // No permitir dibujar hasta que no presione el botón del mouse 
+ let estaDibujando = false;
 
-	// La posición del cursor se actualiza a medida que movemos el mouse alrededor del Canvas 
-	obtenerPosicion(event); 
+ // Desde dónde comenzar una línea y luego dónde terminarla
+ let vectorX = 0;
+ let vectorY = 0;
+ let hue = 0;
 
-	// Se traza una línea desde el inicio 
-	ctx.lineTo(coordenadas.x , coordenadas.y); 
-		
-	// Dibujamos las líneas  
-	ctx.stroke(); 
-} 
+ function dibujo(evento) {
+
+ 	//Solo permitir dibujar haciendo clic y arrastrando el puntero del mouse 
+ 	if (!estaDibujando)
+ 		return;
+
+ 	console.log(evento);
+
+ 	contenedor.beginPath();
+ 	// El cursor para comenzar a dibujar se mueve a esta coordenada        
+ 	contenedor.moveTo(vectorX, vectorY);
+
+ 	// Se traza una línea desde el inicio 
+ 	contenedor.lineTo(evento.offsetX, evento.offsetY);
+
+ 	// Dibujamos las líneas 
+ 	contenedor.stroke();
+
+ 	[vectorX, vectorY] = [evento.offsetX, evento.offsetY];
+
+
+ }
+
+ // Eventos del Mouse 
+ canvas.addEventListener('mousemove', dibujo);
+ canvas.addEventListener('mousedown', (evento) => {
+ 	// Permitir dibujar cuando se presiona el botón del mouse 
+ 	estaDibujando = true;
+
+ 	[vectorX, vectorY] = [evento.offsetX, evento.offsetY];
+ });
+
+ // Eventos del Mouse 
+ canvas.addEventListener('mouseup', () => estaDibujando = false);
+ canvas.addEventListener('mouseout', () => estaDibujando = false);
+
+
+ /* Usar Canvas en Dispositivos Móviles */
+
+ // Dibujar al tocar la pantalla del dispositivo móvil
+ document.body.addEventListener("touchstart", function(evento) {
+ 	if (evento.target == canvas) {
+ 		evento.preventDefault();
+ 		clienteX = evento.touches[0].clienteX;
+ 		clienteY = evento.touches[0].clienteY;
+ 		estaDibujando = true;
+ 		dibujo(clienteX, clienteY)
+ 	}
+ }, false);
+
+ // No Dibujar al dejar de tocar la pantalla del dispositivo móvil
+ document.body.addEventListener("touchend", function(evento) {
+ 	if (evento.target == canvas) {
+ 		evento.preventDefault();
+ 		estaDibujando = false;
+ 	}
+ }, false);
+
+ // Permitir desplazarse en la pantalla del dispositivo móvil
+ document.body.addEventListener("touchmove", function(evento) {
+ 	if (evento.target == canvas) {
+ 		evento.preventDefault();
+ 		evento.offsetX = evento.targetTouches[0].clienteX;
+ 		evento.offsetY = evento.targetTouches[0].clienteY;
+ 		dibujo(evento)
+ 	}
+ }, false);
